59789700258ab97f88f54b87cde2947caab6f3a0
Aniket Shrivastava <aniketsrivastava57@gmail.com>
2026-03-04 18:53:42 +0530

    first commit

--- 1.Two Sum.cpp
+++ 1.Two Sum.cpp
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
@@ -17,3 +18,24 @@ public:
        return {};
     }
 };
+=======
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+       int start;
+       int end;
+
+       for(start =0; start<nums.size();start++)
+       {
+        for(end= start+1; end<nums.size(); end++)
+        {
+            if(nums[start] + nums[end] == target){
+                return {start,end};
+                break;
+            }
+        }
+       }
+       return {};
+    }
+};
+>>>>>>> 0d65645 (first commit)
