bd1a78dfadc009f6ea1bcd140e0c704e84048774
Aniket Shrivastava <aniketsrivastava57@gmail.com>
2026-03-05 18:52:02 +0530

    first commit

---
+++ Array/Tow Pointer/167. Two Sum II - Input Array Is Sorted.cpp
@@ -0,0 +1,23 @@
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+       int n= nums.size();
+       int start=0;
+       int end=n-1;
+       while(start< end){
+        int sum = nums[start] + nums[end];
+        if(sum == target){
+            return {start +1, end+1};
+        }
+        else if(sum >target){
+            end--;
+        }
+        else{
+            start++;
+        }
+       }
+
+       return {-1,-1};
+    }
+};
